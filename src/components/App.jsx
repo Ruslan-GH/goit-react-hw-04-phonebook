cc3afa41d0b74d2e4a37bdeb0f6e7fb868616d64
Ruslan-GH <kr.safetyok@gmail.com>
2023-02-19 21:17:54 +0200

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,89 +1,68 @@
-import { nanoid } from 'nanoid';
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import PhonebookForm from '../components/PhonebookForm';
 import Filter from './Filter';
 import Contacts from './Contacts/Contacts';
 
-export class App extends Component {
-  state = {
-    contacts: [
+const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(window.localStorage.getItem('contacts')) ?? [
       { id: '1', name: 'Rosie Simpson', number: '+ 459-12-56' },
       { id: '2', name: 'Hermione Kline', number: '+ 443-89-12' },
-      { id: '3', name: 'Eden Clements', number: '+ 645-17-79' },
-      { id: '4', name: 'Annie Copeland', number: '+ 227-91-26' },
-    ],
-    filter: '',
+    ]
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const deleteContact = contactId => {
+    setContacts(state => state.filter(contact => contact.id !== contactId));
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-  addContact = newContact => {
-    const checkedName = this.state.contacts.find(
+  const addContact = newContact => {
+    const checkedName = contacts.find(
       contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
     );
 
     if (checkedName) {
-      alert('This Name already exist!' + newContact.name);
+      alert('This Name already exist!');
       return;
     }
 
-    this.setState(prevState => ({
-      contacts: [newContact, ...prevState.contacts],
-    }));
+    setContacts(prevState => [newContact, ...contacts]);
   };
 
-  findDuplicate = newContactName => {
-    const isDublicate = this.state.contacts.some(
+  const findDuplicate = newContactName => {
+    const isDublicate = contacts.some(
       contact => contact.name === newContactName
     );
 
     if (isDublicate) {
-      alert('This Name already exist!' + newContactName);
+      return alert('This Name already exist!');
     }
   };
 
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
   };
 
-  getFilterContact = () => {
-    const { contacts, filter } = this.state;
+  const getFilterContact = () => {
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
+  return (
+    <div style={{ marginLeft: '40px' }}>
+      <h1>Phonebook</h1>
+      <PhonebookForm onSubmit={addContact} />
+      <h1>Contacts</h1>
+      <Filter value={filter} onChange={changeFilter} />
+      <Contacts contacts={getFilterContact()} onDeleteContact={deleteContact} />
+    </div>
+  );
+};
 
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  }
-
-  render() {
-    const filterContact = this.getFilterContact();
-    return (
-      <div style={{ marginLeft: '40px' }}>
-        <h1>Phonebook</h1>
-        <PhonebookForm onSubmit={this.addContact} />
-        <h1>Contacts</h1>
-        <Filter value={this.state.filter} onChange={this.changeFilter} />
-        <Contacts
-          contacts={filterContact}
-          onDeleteContact={this.deleteContact}
-        />
-      </div>
-    );
-  }
-}
+export default App;
--- src/components/PhonebookForm/PhonebookForm.js
+++ src/components/PhonebookForm/PhonebookForm.js
@@ -1,69 +1,67 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import s from './PhonebookForm.module.css';
 import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
 
-class PhonebookForm extends Component {
-  state = {
-    name: '',
-    number: '',
+const PhonebookForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleNameChange = e => {
+    setName(e.target.value);
   };
 
-  handleChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
+  const handleNumberChange = e => {
+    setNumber(e.target.value);
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const { name, number } = this.state;
-
     const newContact = {
       id: nanoid(),
       name,
       number,
     };
 
-    this.props.onSubmit(newContact);
-    this.setState({ name: '', number: '' });
+    onSubmit(newContact);
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <form className={s.Form} onSubmit={this.handleSubmit}>
-        <label className={s.Label}>
-          Name
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            value={this.state.name}
-            onChange={this.handleChange}
-            placeholder="enter name, pls"
-          />
-        </label>
-        <label className={s.Label}>
-          Number
-          <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            value={this.state.number}
-            onChange={this.handleChange}
-            placeholder="enter number, pls"
-          />
-        </label>
-        <button type="submit" className={s.Button}>
-          Add contact
-        </button>
-      </form>
-    );
-  }
-}
+  return (
+    <form className={s.Form} onSubmit={handleSubmit}>
+      <label className={s.Label}>
+        Name
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          onChange={handleNameChange}
+          placeholder="enter name, pls"
+        />
+      </label>
+      <label className={s.Label}>
+        Number
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+          onChange={handleNumberChange}
+          placeholder="enter number, pls"
+        />
+      </label>
+      <button type="submit" className={s.Button}>
+        Add contact
+      </button>
+    </form>
+  );
+};
 
 PhonebookForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
